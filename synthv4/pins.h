29d0ddcbc5a72eaef0c7d739dfa83a1f10a3f9a3
joshua02 <79498135+joshua02@users.noreply.github.com>
2024-04-01 17:06:29 -0500

    Add files via upload

---
+++ synthv4/pins.h
@@ -0,0 +1,51 @@
+#ifndef PINS
+#define PINS
+
+#define CLK 1
+#define DIN 2
+#define LOAD 3
+
+#define KEY1 28
+#define KEY2 29
+#define KEY3 30
+#define KEY4 31
+#define KEY5 32
+#define KEY6 0
+#define KEY7 0
+#define KEY8 0
+#define KEY9 0
+#define KEY10 0
+#define KEY11 0
+#define KEY12 0
+#define KEY13 0
+#define KEY14 0
+#define KEY15 0
+#define KEY16 0
+#define KEY17 0
+#define KEY18 0
+#define KEY19 0
+#define KEY20 0
+#define KEY21 0
+#define KEY22 0
+#define KEY23 0
+#define KEY24 0
+#define KEY25 0
+
+#define ENCODER1 11
+#define ENCODER2 12
+#define MENUBUTTON 0
+
+#define LCD_E  5
+#define LCD_RS 4
+#define LCD_D7 9
+#define LCD_D6 8
+#define LCD_D5 7
+#define LCD_D4 6
+#define LCD_RW 10
+
+int keyPins[25] = {KEY1, KEY2, KEY3, KEY4, KEY5, KEY6, KEY7, KEY8, KEY9, KEY10, KEY11, KEY12, KEY13, KEY14, KEY15, KEY16, KEY17, KEY18, KEY19, KEY20, KEY21, KEY22, KEY23, KEY24, KEY25};
+
+#endif
+
+
+
